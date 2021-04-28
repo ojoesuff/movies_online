8663b261165cdb6ccf68eb2a2ac11622ffa747f6
Eoin Hearne <eoin.hearne@errigal.com>
2021-04-28 20:21:18 +0100

    getting wishlists and movies - needs more work

--- src/components/wishListDetail/index.js
+++ src/components/wishListDetail/index.js
@@ -1,43 +1,85 @@
 import Grid from '@material-ui/core/Grid';
 import { makeStyles } from "@material-ui/core/styles";
-import { Typography } from "@material-ui/core";
+import { Accordion, AccordionDetails, AccordionSummary, Typography } from "@material-ui/core";
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { useEffect, useState } from 'react';
 
 
 const useStyles = makeStyles((theme) => ({
-    toolbar: {
-        paddingTop: "5px",
-        overflow: "hidden",
-        marginLeft: 250,
-        width: "80%",
-    }
+    root: {
+        flexGrow: 1,
+        padding: theme.spacing(5), 
+      },
   }));
 
-const WishlistDetail = ({ wishlists }) => {
+const WishlistDetail = ({ wishlists, movies }) => {
     const classes = useStyles();
+    const [movieNames, setMovieNames] = useState([])
+
+    const setWishlistMovies = (wishlistId) => {
+        return movies.map(movie => {
+            
+            movie?.wishlist_ids?.includes(wishlistId) ? 
+            (<Typography>{movie.title}</Typography>)
+            : false
+        }
+        );
+    } 
+    useEffect(() => {
+        setMovieNames(wishlists.map(wishlist => (
+                {id : wishlist.id,
+                movies : movies.map(movie => movie.title)
+                }
+        ) ))       
+    }, []);
+
+    // if(!movieNames) {
+    //     setMovieNames(movies.map(movie =>
+    //         movie.title
+    //     ))
+    // }
     
     return (
     <>
-
-        {wishlists.length > 0 ?        
-        wishlists.map(wishlist => 
-            <Typography>{wishlist.name}</Typography>
-        )
-        :
-        <Typography>No wishlists</Typography>
-        }
-        {/* <div className={classes.toolbar} />
-            
-            {filteredMovies.length > 0 ?
-            <Grid container spacing={2} className={classes.toolbar}>
-            {filteredMovies}
-            </Grid> :
-            <div>
-                <Typography align="center" variant="h5">No Movies Available</Typography>
-            </div>
-            
-            }
-            
-        <div className={classes.toolbar} /> */}
+        <div className={classes.root}>
+            <Grid container spacing={1}>                  
+                {wishlists.length > 0 ?                
+                wishlists.map(wishlist => (
+                <Grid item lg={8} md={8} sm={12} xs={12}>
+                    <Accordion style={{opacity:0.9}}>
+                        <AccordionSummary
+                            expandIcon={<ExpandMoreIcon/>}
+                        >
+                            <Typography>{wishlist.name}</Typography>
+                        </AccordionSummary> 
+                        {movieNames.find(movie => movie.id == wishlist.id) ?
+                        movieNames.find(movie => movie.id == wishlist.id).movies.map(m =>                        
+                        <AccordionDetails>
+                        <Typography>{m}</Typography>
+                        </AccordionDetails>
+                        )
+                        :
+                        <AccordionDetails>
+                        <Typography>Empty</Typography>
+                        </AccordionDetails>
+                        }
+                        {/* {movieNames ? 
+                        movieNames.map(m => 
+                        <AccordionDetails>
+                        <Typography>{m}</Typography>
+                        </AccordionDetails>)
+                        : <AccordionDetails>
+                        <Typography>Empty</Typography>
+                        </AccordionDetails>
+                         
+                        } */}
+                    </Accordion>
+                </Grid>
+                ))
+                : <Typography>No wishlists created. Click the button below...</Typography>
+                }                                     
+            </Grid>            
+        </div>
     </>
         
     )
--- stories/index.js
+++ stories/index.js
@@ -2,7 +2,8 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import RatingBubble from "../src/components/ratingBubble";
 import WishlistDetail from "../src/components/wishListDetail";
-import { MemoryRouter } from "react-router";
+import wishlists from "./sampleData/wishlists.json"
+import movies from "./sampleData/movies.json"
 // import MovieList from "../src/components/movieList";
 // import Header from "../src/components/header";
 // import Footer from "../src/components/footer";
@@ -10,116 +11,7 @@ import { MemoryRouter } from "react-router";
 // import MovieDetail from "../src/components/movieDetail";
 // import AddReviewButton from "../src/components/addReviewButton";
 
-const fightClubMovie = {
-  "adult": false,
-  "backdrop_path": "/rr7E0NoGKxvbkb89eR1GwfoYjpA.jpg",
-  "belongs_to_collection": null,
-  "budget": 63000000,
-  "genres": [
-      {
-          "id": 18,
-          "name": "Drama"
-      }
-  ],
-  "homepage": "http://www.foxmovies.com/movies/fight-club",
-  "id": 550,
-  "imdb_id": "tt0137523",
-  "original_language": "en",
-  "original_title": "Fight Club",
-  "overview": "A ticking-time-bomb insomniac and a slippery soap salesman channel primal male aggression into a shocking new form of therapy. Their concept catches on, with underground \"fight clubs\" forming in every town, until an eccentric gets in the way and ignites an out-of-control spiral toward oblivion.",
-  "popularity": 40.597,
-  "poster_path": "/pB8BM7pdSp6B6Ih7QZ4DrQ3PmJK.jpg",
-  "production_companies": [
-      {
-          "id": 508,
-          "logo_path": "/7PzJdsLGlR7oW4J0J5Xcd0pHGRg.png",
-          "name": "Regency Enterprises",
-          "origin_country": "US"
-      },
-      {
-          "id": 711,
-          "logo_path": "/tEiIH5QesdheJmDAqQwvtN60727.png",
-          "name": "Fox 2000 Pictures",
-          "origin_country": "US"
-      },
-      {
-          "id": 20555,
-          "logo_path": "/hD8yEGUBlHOcfHYbujp71vD8gZp.png",
-          "name": "Taurus Film",
-          "origin_country": "DE"
-      },
-      {
-          "id": 54051,
-          "logo_path": null,
-          "name": "Atman Entertainment",
-          "origin_country": ""
-      },
-      {
-          "id": 54052,
-          "logo_path": null,
-          "name": "Knickerbocker Films",
-          "origin_country": "US"
-      },
-      {
-          "id": 25,
-          "logo_path": "/qZCc1lty5FzX30aOCVRBLzaVmcp.png",
-          "name": "20th Century Fox",
-          "origin_country": "US"
-      },
-      {
-          "id": 4700,
-          "logo_path": "/A32wmjrs9Psf4zw0uaixF0GXfxq.png",
-          "name": "The Linson Company",
-          "origin_country": "US"
-      }
-  ],
-  "production_countries": [
-      {
-          "iso_3166_1": "DE",
-          "name": "Germany"
-      },
-      {
-          "iso_3166_1": "US",
-          "name": "United States of America"
-      }
-  ],
-  "release_date": "1999-10-15",
-  "revenue": 100853753,
-  "runtime": 139,
-  "spoken_languages": [
-      {
-          "english_name": "English",
-          "iso_639_1": "en",
-          "name": "English"
-      }
-  ],
-  "status": "Released",
-  "tagline": "Mischief. Mayhem. Soap.",
-  "title": "Fight Club",
-  "video": false,
-  "vote_average": 8.4,
-  "vote_count": 21594
-}
-
-const wishlists = {
-  "wishlists": [
-      {
-          "id": 0,
-          "name": "Kids"
-      },
-      {
-          "id": 1,
-          "name": "Classics"
-      },
-      {
-          "id": 2,
-          "name": "Scary"
-      },
-  ]
-}
-
 const emptyList = {}
-
 
 storiesOf("Movies/Rating Bubble", module)
   .add("Bad", () => {
@@ -134,10 +26,10 @@ storiesOf("Movies/Rating Bubble", module)
 
 storiesOf("Wishlist/Wishlist Details", module)
   .add("Multiple", () => {
-    return <WishlistDetail wishlists={wishlists.wishlists}/>;
+    return <WishlistDetail wishlists={wishlists.wishlists} movies={movies.results}/>;
   })
   .add("Empty", () => {
-    return <WishlistDetail wishlists={emptyList} />;
+    return <WishlistDetail wishlists={emptyList} movies={movies.results}/>;
   })
 
 // storiesOf("Movies/Movie Card", module)
